1cfe4666e51fca8bdfdd206d6145bf9a9284f582
igorslepenkov <slepenkov.nii@yandex.by>
2023-03-02 12:35:38 +0300

    init(main): init project

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import { config as configEnv } from 'dotenv';
+
+configEnv();
+
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+const PORT = process.env.PORT ?? 3000;
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(PORT);
+}
+bootstrap();
